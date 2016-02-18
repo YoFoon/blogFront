980e1b03fe60328223cfcb1e8aed06d66b3c05b6
YoFoon <605022360@qq.com>
2016-02-18 21:25:56 +0800

    add dist

---
+++ dist/lib/app.js
@@ -0,0 +1,60 @@
+var app = angular.module('myApp', ['ui.router']);
+
+var SERVICE_URL = 'http://127.0.0.1:3000/';
+
+//初始化配置
+app.run(['$rootScope', '$state', '$stateParams', function($rootScope,$state, $stateParams) {
+  $rootScope.$state = $state;
+  $rootScope.$stateParams = $stateParams;
+}]);
+
+//路由配置
+app.config(['$stateProvider', '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/index');
+
+    $stateProvider.
+
+    state('index', {
+      url: '/index',
+      views: {
+        '': {
+            templateUrl: 'view/index/index.html',
+        },
+        'nav@index': {
+            templateUrl: 'view/index/pageShow.html',
+            controller: 'PageShowCtrl'
+        },
+        'con@index': {
+            templateUrl: 'view/list/list.html',
+            controller: ''
+        }
+      }
+    }).
+
+    state('list', {
+      url: '/list',
+      views: {
+        '': {
+            templateUrl: 'view/index/index.html',
+        },
+        'nav@list': {
+            templateUrl: 'view/index/pageShow.html',
+            controller: 'listCtrl'
+        },
+        'con@list': {
+            templateUrl: 'view/list/list.html',
+            controller: ''
+        }
+      }
+    });
+
+  }]);
+app.controller('PageShowCtrl', ['$scope','$timeout',
+  function($scope,$timeout) {
+    $scope.isFullPage = true;
+  }]);
+app.controller('listCtrl', ['$scope','$timeout',
+  function($scope,$timeout) {
+    $scope.isFullPage = false;
+  }]);
